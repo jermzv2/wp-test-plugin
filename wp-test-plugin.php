bbded58f1f3d3ebb8bedfaab890538516a63f3ce
jermzv2 <jermzv2@gmail.com>
2017-01-24 12:44:29 -0800

    Version Change

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Test Plugin
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
-Version: 1.0
+Version: 1.1
 Author: Ugly Duck Marketing
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
