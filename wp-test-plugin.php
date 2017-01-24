de676072816183c527b9938bda11b99b094e38c9
jermzv2 <jermzv2@gmail.com>
2017-01-24 13:18:11 -0800

    Release Asset

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -2,8 +2,9 @@
 /*
 Plugin Name: WP Test Plugin
 Description: A brief description of the Plugin.
-Version: 1.4.1
+Version: 1.4.2
 Author: Ugly Duck Marketing
 License: A "Slug" license name e.g. GPL2
 GitHub Plugin URI: https://github.com/jermzv2/wp-test-plugin
+Release Asset: true
 */
