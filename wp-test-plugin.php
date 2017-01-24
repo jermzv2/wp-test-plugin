795b8ae9f3cd4ece47a1e98c50c1a5ef86eaa3fc
jermzv2 <jermzv2@gmail.com>
2017-01-24 13:38:00 -0800

    Update

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Test Plugin
 Description: A brief description of the Plugin.
-Version: 1.5
+Version: 1.5.1
 Author: Ugly Duck Marketing
 License: A "Slug" license name e.g. GPL2
 GitHub Plugin URI: https://github.com/jermzv2/wp-test-plugin
